af5b32f8d606583a71a8cccadb6e798cd07c4d4c
durga <durga@DESKTOP-RSDEC2U>
2024-10-24 19:04:20 +0530

    Third commit

---
+++ SVKP_college/src/BasicsofJava/DataTypePrimitive.java
@@ -0,0 +1,44 @@
+package BasicsofJava;
+
+public class DataTypePrimitive {
+
+	public static void main(String[] args) 
+	{
+		//byte-1 byte
+		byte bytemax=127;
+		byte bytemin=-128;
+		System.out.println("Min range of byte is" +bytemin+ "Max range of byte is" +bytemax);
+		//Short-2 bytes
+		short ShortMax=32767;
+		short ShortMin=-32768;
+		System.out.println("Min range of Short is" +ShortMin+ "Max range of short is" +ShortMax);
+		//int-4 bytes
+		int MaxInt=2147483647;
+		int MinInt=-2147483648;
+		System.out.println("Min range of int is" +MinInt+ "Max range of int is" +MaxInt);
+		//long -8 bytes
+		long longMax=-9223372036854775807L;
+         long longMin=-9223372036854775808L;
+		System.out.println("Min range of long is"+longMin+"Max range of long is" +longMax);
+		
+				
+		//float-4 bytes //double-8 bytes
+		float f=3224.141243278345f;
+		double d=3456.14124512345678902345678914d;
+		System.out.println("float value" +f+ "double" +d);
+		//boolean
+		boolean status=false;
+		System.out.println("boolean value is" +status);
+		char c='K';
+		System.out.println("char value" +c);
+				
+		
+		
+		
+		
+				                                             
+	
+
+	}
+
+}
