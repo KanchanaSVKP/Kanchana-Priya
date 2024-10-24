7b2d82ede2d11a8f50dd0074d36bbe07bcad5218
durga <durga@DESKTOP-RSDEC2U>
2024-10-24 19:13:24 +0530

    Third commit

---
+++ SVKP_college/src/BasicsofJava/ArrayDemo.java
@@ -0,0 +1,11 @@
+package BasicsofJava;
+
+public class ArrayDemo {
+
+	public static void main(String[] args)
+	{
+	int num[]= {1,23,56,78,89};
+	System.out.println(num[1]);
+	System.out.println(num[4]);
+	}
+}
