a9019495396ef5183eb3f636a2de850b010af775
durga <durga@DESKTOP-RSDEC2U>
2024-10-25 07:53:57 +0530

    Fourth commit

---
+++ SVKP_college/src/BasicsofJava/TypeCastingDemo.java
@@ -0,0 +1,17 @@
+package BasicsofJava;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args)
+	{
+		//implicit Casting
+		byte b=127;
+		short s=b;
+		System.out.println(s);
+		//Explicit Casting
+	    double d=10.5276883;
+	    int i=(int)d;
+	    System.out.println (i); 
+	    }
+
+}
