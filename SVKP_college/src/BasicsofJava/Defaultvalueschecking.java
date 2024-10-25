b9242691cbcb86c25587888f1496f9e13f63a820
durga <durga@DESKTOP-RSDEC2U>
2024-10-25 07:28:30 +0530

    Third commit

---
+++ SVKP_college/src/BasicsofJava/Defaultvalueschecking.java
@@ -0,0 +1,28 @@
+package BasicsofJava;
+class Demo
+{
+		int i;
+		byte b;
+		float f;
+		double d;
+		char c;
+		boolean b1;
+		String str;
+		int a[];
+}
+	public class Defaultvalueschecking {
+
+     public static void main(String[] args)
+			{
+    	 Demo d1=new Demo();
+    	 System.out.println(d1.i);
+    	 System.out.println(d1.b);
+    	 System.out.println(d1.f);
+    	 System.out.println(d1.d);
+    	 System.out.println(d1.c);
+    	 System.out.println(d1.b1);
+    	 System.out.println(d1.str);
+    	 System.out.println(d1.a);
+    	 }
+
+}
