e379446c7b4fbc35b133d646985b29e25aced8dd
durga <durga@DESKTOP-RSDEC2U>
2024-10-27 18:56:20 +0530

    Fifth commit

---
+++ SVKP_college/src/BasicsofJava/WhileDemo.java
@@ -0,0 +1,16 @@
+package BasicsofJava;
+
+public class WhileDemo
+{
+
+	public static void main(String[] args)
+	{
+		int i=3;
+		while(i<=7)
+		{
+			System.out.println("Tomorrow is holiday  "  +i);
+			i++;
+		}
+	}
+}
+
