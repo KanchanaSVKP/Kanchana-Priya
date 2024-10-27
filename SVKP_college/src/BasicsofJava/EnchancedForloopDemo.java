c543ed5620c5ef81f508874db32089f9457b0275
durga <durga@DESKTOP-RSDEC2U>
2024-10-27 19:28:56 +0530

    Fifth commit

---
+++ SVKP_college/src/BasicsofJava/EnchancedForloopDemo.java
@@ -0,0 +1,13 @@
+package BasicsofJava;
+
+public class EnchancedForloopDemo {
+
+	public static void main(String[] args) {
+		int num[]= {1,4,5,6,7,4,3,7};
+		for(int numbers:num)
+		{
+			System.out.println(numbers);
+		}
+	}
+
+}
