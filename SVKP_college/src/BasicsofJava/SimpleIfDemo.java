bf4e018103a24926b8b8146a078a2f3e7ee20073
durga <durga@DESKTOP-RSDEC2U>
2024-10-27 18:08:09 +0530

    Fifth Commit

---
+++ SVKP_college/src/BasicsofJava/SimpleIfDemo.java
@@ -0,0 +1,16 @@
+package BasicsofJava;
+
+public class SimpleIfDemo {
+
+	public static void main(String[] args)
+	{
+		String place="India";
+		if( place == "India")
+		{
+			System.out.println("Ready to go");
+		}
+			
+	}
+		
+
+}
