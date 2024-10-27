4fe2d537337ae4858035db19d3ca9f4e83171a4f
durga <durga@DESKTOP-RSDEC2U>
2024-10-27 19:52:49 +0530

    Fifth commit

---
+++ SVKP_college/src/BasicsofJava/ReturnDemo.java
@@ -0,0 +1,17 @@
+package BasicsofJava;
+
+  public class ReturnDemo 
+   {
+   public static void main(String[] args)
+	{
+	System.out.println(sum(15,15));
+	}
+	static int sum (int a,int b)
+			{
+				return a*b;
+			}
+    }
+	
+
+
+		
