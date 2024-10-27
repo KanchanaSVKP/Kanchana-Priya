bf4944ea399f4290bef862a640c6e6f891faf067
durga <durga@DESKTOP-RSDEC2U>
2024-10-27 18:12:40 +0530

    Fifth commit

---
+++ SVKP_college/src/BasicsofJava/IfelseDemo.java
@@ -0,0 +1,17 @@
+package BasicsofJava;
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+		String place="USA";
+		if( place == "India")
+		{
+			System.out.println("Ready to go");
+		}
+		else
+		{
+			System.out.println("Not Ready to go");
+		}
+	}
+
+}
