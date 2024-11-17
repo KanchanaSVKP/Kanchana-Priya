4939fdb99593fc979e3b1f9391725ca50fd7acea
durga <durga@DESKTOP-RSDEC2U>
2024-11-17 18:00:06 +0530

    Eighth commit

---
+++ SVKP_college/src/org/tnsif/capgemimi/c2tc/Basicsofjava/Built_in_packages.java
@@ -0,0 +1,15 @@
+package org.tnsif.capgemimi.c2tc.Basicsofjava;
+
+public class Built_in_packages {
+
+	public static void main(String[] args)
+	{
+		String name="kanchana priya";
+		System.out.println("Original string" +name);
+		System.out.println("Uppercase string "+name.toUpperCase()); 
+		
+		int max=Math.max(29,70);
+		System.out.println(max);
+		}
+
+}
