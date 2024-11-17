ec7d44ea51275e3548b4a4f01ee4674ded9f08ec
durga <durga@DESKTOP-RSDEC2U>
2024-11-17 18:31:18 +0530

    Ninth commit

---
+++ SVKP_college/src/org/tnsif/capgemini/c2tc/Sr_Developer/KIA_Internals.java
@@ -0,0 +1,15 @@
+package org.tnsif.capgemini.c2tc.Sr_Developer;
+import org.tnsif.capgemini.c2tc.Jr_Developer.*;
+class KIA_Internals {
+
+	public static void main(String[] args) 
+	{
+		KIA_Airbags obj=new KIA_Airbags();
+		obj.disp_Airbags();
+		 KIA_Speed_limit obj1=new KIA_Speed_limit();
+		obj1.disp_Speed_limit();
+		
+
+	}
+
+}
